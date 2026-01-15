610a5d35586b42e81f14d7e0084d46970e6e53cf
Syed Burhan Ahmed <syedburhanahmedd@gmail.com>
2026-01-16 01:00:49 +0500

    Fix: Update next-themes import path for v0.4+

--- src/components/providers/ThemeProvider.tsx
+++ src/components/providers/ThemeProvider.tsx
@@ -1,8 +1,7 @@
-'use client';
+"use client";
 
 import * as React from 'react';
-import { ThemeProvider as NextThemesProvider } from 'next-themes';
-import { type ThemeProviderProps } from 'next-themes/dist/types';
+import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from 'next-themes';
 
 export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children, ...props }) => {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
